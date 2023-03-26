272080be90d8149967da12dfad17701ad693fbe7
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2023-03-26 09:35:11 -0500

    Even prettier

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -1,130 +1,8 @@
 var app = new Vue({
   el: '#app',
   data: {
-    races: [
-      {
-        title: "Bahrain",
-        startDate: "2023-03-03",
-        endDate: "2023-03-05",
-        round: 1,
-        laps: 57,
-        raceName: "Formula 1 Gulf Air Bahrain Grand Prix 2023",
-        trackName: "Bahrain International Circuit",
-        countryCode: "bh",
-        km: 5.412,
-        trackFile: "bahrain.svg"
-      },
-      {
-        title: "Saudi Arabia",
-        startDate: "2023-03-17",
-        endDate: "2023-03-19",
-        round: 2,
-        laps: 50,
-        raceName: "Formula 1 STC Saudi Arabian Grand Prix 2023",
-        trackName: "Jeddah Corniche Circuit",
-        countryCode: "sa",
-        km: 6.174,
-        trackFile: "saudi-arabia.svg"
-      },
-      {
-        title: "Australia",
-        startDate: "2023-03-31",
-        endDate: "2023-04-02",
-        round: 3,
-        laps: 58,
-        raceName: "Formula 1 Rolex Australian Grand Prix 2023",
-        trackName: "Albert Park Circuit",
-        countryCode: "au",
-        km: 5.278,
-        trackFile: "australia.svg"
-      },
-      {
-        title: "Azerbaijan",
-        startDate: "2023-04-28",
-        endDate: "2023-04-30",
-        round: 4,
-        laps: 0,
-        raceName: "Formula 1 Azerbaijan Grand Prix 2023",
-        trackName: "Baku City Circuit",
-        countryCode: "az",
-        km: 4.909,
-        trackFile: "azerbaijan.svg"
-      },
-      {
-        title: "Miami",
-        startDate: "2023-05-05",
-        endDate: "2023-05-07",
-        round: 5,
-        laps: 0,
-        raceName: "Formula 1 Crypto.com Miami Grand Prix 2023",
-        trackName: "Miami International Autodrome",
-        countryCode: "us",
-        km: 4.909,
-        trackFile: "miami.svg",
-        customEndWeight: 48,
-        customMapWidth: 56
-      },
-      {
-        title: "Emilia-Romagna",
-        startDate: "2023-05-19",
-        endDate: "2023-05-21",
-        round: 6,
-        laps: 0,
-        raceName: "Formula 1 Qatar Airways Gran Premio del Made in Italy e Dell’Emila-Romagna 2023",
-        trackName: "Autodromo Enzo e Dino Ferrari",
-        countryCode: "it",
-        km: 4.909,
-        trackFile: "emilia.svg",
-        customEndWeight: 25,
-      },
-      {
-        title: "Italy",
-        startDate: "2023-10-01",
-        endDate: "2023-10-03",
-        round: 15,
-        laps: 0,
-        raceName: "Formula 1 Pirelli gran premio d’italia 2023",
-        trackName: "autodromo nazionale monza",
-        countryCode: "it",
-        km: 4.909,
-        customStartWeight: 120,
-      },
-      {
-        title: "Great Britain",
-        startDate: "2023-06-07",
-        endDate: "2023-06-09",
-        round: 11,
-        laps: 0,
-        raceName: "Formula 1 Aramco British Grand Prix 2023",
-        trackName: "Silverstone Circuit",
-        countryCode: "it",
-        km: 4.909
-      },
-      {
-        title: "Japan",
-        startDate: "2023-09-22",
-        endDate: "2023-09-24",
-        round: 17,
-        laps: 0,
-        raceName: "Formula 1 lenovo japanese grand prix 2023",
-        trackName: "suzuka international racing course",
-        countryCode: "jp",
-        km: 4.909,
-        customStartWeight: 86,
-        customEndWeight: 60
-      },
-      {
-        title: "Las Vegas",
-        startDate: "2023-10-16",
-        endDate: "2023-10-18",
-        round: 22,
-        laps: 0,
-        raceName: "Formula 1 Heineken silver Las vegas grand prix 2023",
-        trackName: "Las vegas",
-        countryCode: "us",
-        km: 4.909
-      },
-    ],
+    
+    races: races,
 
     letterTests: [
       {
@@ -355,14 +233,10 @@ var app = new Vue({
           <time>${endMonth} ${endDay}</time>
         `;
       }
-
-
-      
-      //return d.getMonth();
     },
 
-    animateRace(ev) {
-      console.log(ev.target);
+    padNumber(number,padAmount) {
+      return String(number).padStart(2,'0')
     }
   },
 
--- js/site.js
+++ js/site.js
@@ -2,5 +2,6 @@
 
 
 //@prepros-append partials/_variables.js
+//@prepros-append partials/_races.js
 //@prepros-append partials/_actions.js 
 //@prepros-append partials/_vue.js
